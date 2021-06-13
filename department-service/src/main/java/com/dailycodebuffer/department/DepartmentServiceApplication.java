e8e96a3ea23d59f5cf767031b54b01dd5cfa1119
rafu07 <77190565+rafu07@users.noreply.github.com>
2021-06-13 23:25:45 +0200

    Add files via upload

---
+++ department-service/src/main/java/com/dailycodebuffer/department/DepartmentServiceApplication.java
@@ -0,0 +1,19 @@
+package com.dailycodebuffer.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/main/java/com/dailycodebuffer/user/UserServiceApplication.java
@@ -0,0 +1,35 @@
+package com.dailycodebuffer.user;
+
+import com.dailycodebuffer.user.VO.Department;
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
+
+
